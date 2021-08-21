871d6fb7a644ced81a4507445bbf855505070d9c
Lajto Mekadimon <85128868+lajtomekadimon@users.noreply.github.com>
2021-08-21 11:15:28 +0200

    Create Languages template of Admin Panel.

--- src/templates/admin/languages.rs
+++ src/templates/admin/languages.rs
@@ -1,5 +1,6 @@
 use markup;
 
+use crate::i18n::t::t;
 use crate::templates::admin_layout::AdminLayout;
 use crate::templates::widgets::admin_panel::AdminPanel;
 
@@ -13,15 +14,79 @@ markup::define! {
             title: title,
             lang_code: lang_code,
             content: AdminPanel {
-                content: Content {},
+                content: Content {
+                    lang_code: lang_code,
+                },
                 current_page: "languages",
                 lang_code: lang_code,
             },
         }
     }
 
-    Content() {
-        "Bla"
+    Content<'a>(
+        lang_code: &'a str,
+    ) {
+        h1[class = "title"] {
+            {&t("Languages", lang_code)}
+
+            a[
+                href = "/{lang}/admin/new_language".replace("{lang}", lang_code),
+                class = "button is-link is-pulled-right has-text-weight-normal",
+            ] {
+                {&t("Add language", lang_code)}
+            }
+        }
+
+        table[
+            class = "table is-bordered is-hoverable is-fullwidth",
+        ] {
+            thead {
+                tr {
+                    th {
+                        {&t("Language", lang_code)}
+                    }
+                    th {
+                        {&t("Code", lang_code)}
+                    }
+                    th {
+                        {&t("Flag", lang_code)}
+                    }
+                    th {
+                        {&t("Last update", lang_code)}
+                    }
+                }
+            }
+            tbody {
+                tr {
+                    td {
+                        a[
+                            href = "/{lang}/admin/edit_language?id=".replace("{lang}", lang_code),
+                        ] {
+                            "English"
+                        }
+                    }
+                    td { "en" }
+                    td {
+                        img[src = "https://lajto.com/wp-content/uploads/2021/02/me.png"];
+                    }
+                    td { "2021-08-21 10:37" }
+                }
+                tr {
+                    td {
+                        a[
+                            href = "/{lang}/admin/edit_language?id=".replace("{lang}", lang_code),
+                        ] {
+                            "Spanish"
+                        }
+                    }
+                    td { "es" }
+                    td {
+                        img[src = "https://lajto.com/wp-content/uploads/2021/02/me.png"];
+                    }
+                    td { "2021-08-21 10:37" }
+                }
+            }
+        }
     }
 }
 
