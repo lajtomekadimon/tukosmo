b796c603ef2f2638d9116ceca10ebad2a38fcecd
Lajto Mekadimon <85128868+lajtomekadimon@users.noreply.github.com>
2021-10-07 15:03:42 +0200

    Implement database handler types in awa_sessions().

--- src/templates/admin/sessions.rs
+++ src/templates/admin/sessions.rs
@@ -2,21 +2,21 @@ use markup;
 
 use crate::templates::admin_layout::AdminLayout;
 use crate::templates::widgets::admin_panel::AdminPanel;
-use crate::database::types::AdminDataDB;
+use crate::handlers::admin::sessions::SessionsAResponse;
 
 
 markup::define! {
     Sessions<'a>(
         title: &'a str,
-        data: &'a AdminDataDB,
+        q: &'a SessionsAResponse,
     ) {
         @AdminLayout {
             title: title,
-            data: data,
+            data: &q.data,
             content: AdminPanel {
                 content: Content {},
                 current_page: "sessions",
-                data: data,
+                data: &q.data,
             },
         }
     }
