95eef113b24f4b680e01178562bf215d60019cc1
Lajto Mekadimon <85128868+lajtomekadimon@users.noreply.github.com>
2021-10-08 11:47:17 +0200

    Fix err_field_is_not_lang_code().

--- db/extra/e_is_lang_code.sql
+++ db/extra/e_is_lang_code.sql
@@ -15,7 +15,7 @@ AS $$
 SELECT (text_value ~ '^[a-z]+$' AND CHAR_LENGTH(text_value) = 2)
     OR (
         SUBSTRING(text_value FOR 2) ~ '^[a-z]+$' AND
-        SUBSTRING(text_value FROM 3 FOR 3) = '-' AND
+        SUBSTRING(text_value FROM 3 FOR 1) = '-' AND
         SUBSTRING(text_value FROM 4) ~ '^[a-z]+$' AND
         CHAR_LENGTH(text_value) = 5
     )
