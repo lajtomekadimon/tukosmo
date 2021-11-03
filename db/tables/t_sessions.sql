1b3d559e9c29809a6c3b91d22d7cff9d88676e6c
Lajto Mekadimon <85128868+lajtomekadimon@users.noreply.github.com>
2021-11-03 19:16:06 +0100

    Add CSRF tokens to user sessions.

--- db/tables/t_sessions.sql
+++ db/tables/t_sessions.sql
@@ -5,6 +5,7 @@ CREATE TABLE t_sessions (
     ts_user       BIGINT      NOT NULL
                               REFERENCES t_users ON DELETE CASCADE,
     ts_user_agent TEXT        NOT NULL,
+    ts_csrf_token UUID        NOT NULL UNIQUE DEFAULT GEN_RANDOM_UUID(),
     ts_date       TIMESTAMPTZ NOT NULL DEFAULT NOW()
 
 );
